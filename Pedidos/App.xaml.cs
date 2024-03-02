042f7a4c37f5e2b7adca1c1701dd165391bf3a4b
Luis Taveras <luistaveras74@gmail.com>
2024-03-02 16:27:42 -0400

    Agregar archivos de proyecto.

---
+++ Pedidos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pedidos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Pedidos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Pedidos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
